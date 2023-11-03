7767b13721a2f19fbaa3c082247e6435ff87b480
Obedkov Vladislav <230765@astanait.edu.kz>
2023-11-03 20:02:20 +0600

    Test

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -1,36 +1,36 @@
-declare module "next-auth" {
-    interface Session {
-        user: {
-            email: string;
-            // sub: {
-            //     id: number;
-            // }
-            id: number;
-            name?: string;
-            age: number;
-        };
+// declare module "next-auth" {
+//     interface Session {
+//         user: {
+//             email: string;
+//             // sub: {
+//             //     id: number;
+//             // }
+//             id: number;
+//             name?: string;
+//             age: number;
+//         };
 
-        backendTokens: {
-            accessToken: string;
-            refreshToken: string;
-            expiresIn: number;
-        }
-    }
-    type NextAuthOptions = any;
-    // type getServerSession = any;
-}
+//         backendTokens: {
+//             accessToken: string;
+//             refreshToken: string;
+//             expiresIn: number;
+//         }
+//     }
+//     type NextAuthOptions = any;
+//     // type getServerSession = any;
+// }
 
-declare module "next-auth/jwt" {
-    interface JWT {
-        user: {
-            id: number;
-            name: string;
-            email: string;
-        },
-        backendTokens: {
-            accessToken: string;
-            refreshToken: string;
-            expiresIn: number;
-        }
-    }
-}
+// declare module "next-auth/jwt" {
+//     interface JWT {
+//         user: {
+//             id: number;
+//             name: string;
+//             email: string;
+//         },
+//         backendTokens: {
+//             accessToken: string;
+//             refreshToken: string;
+//             expiresIn: number;
+//         }
+//     }
+// }
